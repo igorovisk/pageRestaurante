066c77207bd81c003966f78edf969ce87df7e270
igorovisk <igorovisk@gmail.com>
2021-06-23 22:39:58 -0300

    first

---
+++ js/script.js
@@ -0,0 +1,25 @@
+var app = angular.module("App", ["ngRoute"]);
+
+app.config(function($routeProvider) {
+
+    $routeProvider
+    
+        .when("/home", {
+            templateUrl: "./template/home.html"
+        })
+        .when("/menu", {
+            templateUrl: "./template/menu.html"
+        })
+        .when("/all-organic", {
+            templateUrl: "./template/all-organic.html"
+        })
+        .when("/about-us", {
+            templateUrl: "./template/about-us.html"
+        })
+        .when("/reservation", {
+            templateUrl: "./template/reservation.html"
+        })
+       
+       
+    .otherwise({ redirectTo: './template' })
+})
